97dfcdad7dacf3388ffedac4e10af04f7620132e
Brian Dunlap <briandunlap589@gmail.com>
2015-10-25 15:30:19 -0400

    Add and style nav bar, breadcrumbs, and images to etsy results page. Add a count of search results to etsiest file.

--- lib/etsiest.rb
+++ lib/etsiest.rb
@@ -4,27 +4,28 @@ require 'sinatra/base'
 
 require 'etsiest/version'
 
-Etsy.api_key = ENV['ETSY_KEY']
+Etsy.api_key = ENV["ETSY_KEY"]
 
 module Etsiest
   class App < Sinatra::Base
-    set: logging, true
 
-    get '/search' do
-    results = Etsy::Request.get('/listings/active', :includes => ['Images', 'Shop'], 
-                               :keywords => 'whiskey')
+    get "/search" do
+    results = Etsy::Request.get("/listings/active", :includes => ["Images", "Shop"], 
+                               :keywords => "whiskey")
     results = results.result
+    results_num = results.count
     @results_data = []
       results.each do |result|
         result = {
                    title: result["title"],
                    price: result["price"],
-                   image: result["Images"][0]["url_170x135"]  ,
+                   image: result["Images"][0]["url_fullxfull"],
+                   currency_code: result["currency_code"],
                    vendor: result["Shop"]["shop_name"]
                  }
         @results_data.push(result)
       end
-    erb :index, locals: { results: @results_data }
+    erb :index, locals: { results: @results_data, results_num: results_num }
     end
 
     run! if app_file == $0 
